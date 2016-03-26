bf297f104d53e4fc6ee6ea6fe4df60acdd59fc8c
Olle <ollehu@gmail.com>
2016-03-26 13:05:19 +0100

    Loading

--- CoffeeControl/ViewController.swift
+++ CoffeeControl/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  CoffeeControl
-//  
+//
 //  Olle
 
 import UIKit
@@ -14,57 +14,88 @@ class ViewController: UIViewController {
     @IBOutlet weak var manualControls: UILabel!
     @IBOutlet weak var messageField: UITextView!
     
+    @IBOutlet weak var onPicker: UIDatePicker!
+    @IBOutlet weak var offPicker: UIDatePicker!
     @IBOutlet weak var timerOnField: UILabel!
     @IBOutlet weak var timerOffField: UILabel!
     
     var sshWrapper = SSHWrapper()
+    let alertController = UIAlertController(title: nil, message: "Please wait\n\n", preferredStyle: UIAlertControllerStyle.Alert)
     
-    @IBAction func coffeeOn(sender: AnyObject) {
-        let sshWrapper = SSHWrapper()
-        var error: NSError?
+    let spinnerIndicator: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.WhiteLarge)
+    
+    @IBAction func updateTimer(sender: AnyObject) {
         
-        sshWrapper.connectToHost("192.168.0.126", port: 22, user: "pi", password: "raspberry", error: &error)
+        let offLine = pickerToString(offPicker) + " * * * /usr/bin/python /home/pi/CoffeeControl/offCoffee.py"
+        let onLine = pickerToString(onPicker) + " * * * /usr/bin/python /home/pi/CoffeeControl/onCoffee.py"
         
-        if error != nil {
-            let alertController = UIAlertController(title: "Error", message: error!.localizedDescription, preferredStyle: .Alert)
-            alertController.addAction(UIAlertAction(title: "Ok", style: .Cancel, handler: nil))
-            self.presentViewController(alertController, animated: true, completion: nil)
+        do {
+            try sshWrapper.executeCommand("rm /home/pi/CoffeeControl/cronTemp")
         }
-        else {
-            do {
-                try sshWrapper.executeCommand("gpio write 1 1")
-            }
-            catch {
-                
-            }
+        catch {
         }
+        
+        do {
+            try sshWrapper.executeCommand("echo '" + offLine + "' >> /home/pi/CoffeeControl/cronTemp")
+        }
+        catch {
+        }
+        
+        do {
+            try sshWrapper.executeCommand("echo '" + onLine + "' >> /home/pi/CoffeeControl/cronTemp")
+        }
+        catch {
+        }
+        
+        do {
+            try sshWrapper.executeCommand("sudo crontab /home/pi/CoffeeControl/cronTemp")
+        }
+        catch {
+        }
+        
+        refreshTimer()
+        
+        
     }
-
+    
     @IBAction func light1Changed(sender: AnyObject) {
-        switch (self.onOffLight1.selectedSegmentIndex) {
-        case 0:
+        
+        let seconds = 0.1
+        let delay = seconds * Double(NSEC_PER_SEC)
+        let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
 
+        self.presentViewController(self.alertController, animated: false, completion: nil)
+
+        dispatch_after(dispatchTime, dispatch_get_main_queue(), {
+            
+            switch (self.onOffLight1.selectedSegmentIndex) {
+            case 0:
+                
                 do {
-                    try sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/onLight1.py")
+                    try self.sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/CoffeeControl/onLight1.py")
                 }
                 catch {
                     
                 }
-        case 1:
-
+            case 1:
+                
                 do {
-                    try sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/offLight1.py")
+                    try self.sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/CoffeeControl/offLight1.py")
                 }
                 catch {
                     
                 }
+                
+                
+                
+            default:
+                break
+            }
             
-            
-            
-        default:
-            break
-    }
-    
+        })
+        
+        self.dismissViewControllerAnimated(false, completion: nil)
+
     }
     
     @IBAction func light2Changed(sender: AnyObject) {
@@ -72,7 +103,7 @@ class ViewController: UIViewController {
         case 0:
             
             do {
-                try sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/onLight2.py")
+                try sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/CoffeeControl/onLight2.py")
             }
             catch {
                 
@@ -80,7 +111,7 @@ class ViewController: UIViewController {
         case 1:
             
             do {
-                try sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/offLight2.py")
+                try sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/CoffeeControl/offLight2.py")
             }
             catch {
                 
@@ -95,17 +126,17 @@ class ViewController: UIViewController {
         switch (self.onOffCoffee.selectedSegmentIndex) {
         case 0:
             do{
-            try sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/onCoffee.py")
+                try sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/CoffeeControl/onCoffee.py")
             } catch {
                 
             }
         case 1:
             do{
-            try sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/offCoffee.py")
+                try sshWrapper.executeCommand("sudo /usr/bin/python /home/pi/CoffeeControl/offCoffee.py")
             } catch {
                 
             }
-
+            
         default:
             break
         }
@@ -114,8 +145,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.title = "Coffee Control"
+        
+        spinnerIndicator.center = CGPointMake(135.0, 65.5)
+        spinnerIndicator.color = UIColor.blackColor()
+        spinnerIndicator.startAnimating()
+        
+        alertController.view.addSubview(spinnerIndicator)
         
         sshWrapper = SSHWrapper()
         var error: NSError?
@@ -129,8 +166,9 @@ class ViewController: UIViewController {
         }
         else {
             refreshTimer()
+            refreshPicker()
         }
-
+        
     }
     
     func refreshTimer(){
@@ -145,17 +183,52 @@ class ViewController: UIViewController {
         
         var hourRange = result.startIndex.advancedBy(3)..<result.startIndex.advancedBy(5)
         var minRange = result.startIndex.advancedBy(0)..<result.startIndex.advancedBy(2)
-        var hour = result.substringWithRange(hourRange)
-        var min = result.substringWithRange(minRange)
+        var hour = result.substringWithRange(hourRange).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        var min = result.substringWithRange(minRange).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
         let timerOff = hour + "h " + min + "m"
         
-        hourRange = result.startIndex.advancedBy(54)..<result.startIndex.advancedBy(56)
-        minRange = result.startIndex.advancedBy(51)..<result.startIndex.advancedBy(53)
-        hour = result.substringWithRange(hourRange)
-        min = result.substringWithRange(minRange)
+        hourRange = result.startIndex.advancedBy(67)..<result.startIndex.advancedBy(69)
+        minRange = result.startIndex.advancedBy(64)..<result.startIndex.advancedBy(66)
+        hour = result.substringWithRange(hourRange).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        min = result.substringWithRange(minRange).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
         let timerOn = hour + "h " + min + "m"
         
         timerOnField.text = timerOn
         timerOffField.text = timerOff
+    }
+    
+    func refreshPicker(){
+        var result: String!
+        do {
+            result = try sshWrapper.executeCommand("sudo crontab -l")
+        }
+        catch {
+            result = "Error"
+            
+        }
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        
+        var hourRange = result.startIndex.advancedBy(3)..<result.startIndex.advancedBy(5)
+        var minRange = result.startIndex.advancedBy(0)..<result.startIndex.advancedBy(2)
+        var hour = result.substringWithRange(hourRange).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        var min = result.substringWithRange(minRange).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        offPicker.date = dateFormatter.dateFromString(hour + ":" + min)!
+        
+        hourRange = result.startIndex.advancedBy(67)..<result.startIndex.advancedBy(69)
+        minRange = result.startIndex.advancedBy(64)..<result.startIndex.advancedBy(66)
+        hour = result.substringWithRange(hourRange).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        min = result.substringWithRange(minRange).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        onPicker.date = dateFormatter.dateFromString(hour + ":" + min)!
+    }
+    
+    func pickerToString(datePicker:UIDatePicker) -> String
+    {
+        let date = datePicker.date
+        
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([NSCalendarUnit.Hour, NSCalendarUnit.Minute] , fromDate: date)
+        
+        return "\(components.minute) \(components.hour)"
     }
 }
